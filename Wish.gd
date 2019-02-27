72d35cbe95c158aa8731a70b6e40f48ed9a1c898
James Andrew Pohadi <jamesandrewpohadi@gmail.com>
2019-02-27 23:49:43 +0800

    integrated Chat, Wish and Market

--- Wish.gd
+++ Wish.gd
@@ -1,32 +1,32 @@
 extends Node2D
 
 const GameListItem = preload("GameListItem.tscn")
+onready var social = get_parent()
 
 func _ready():
-	#for i in range(20):
-		#addGlobal("Item " + str(i), "resources")
-	pass
+	generateItemList("Ore")
 
 func addGlobal(name):
 	var global = GameListItem.instance()
 	global.get_node("Name").text = name
-	$ItemList/ScrollContainer/VBoxContainer.add_child(global)
+	$Main/ScrollContainer/VBoxContainer.add_child(global)
 	
 func generateItemList(category):
 	match category:
 		"Ore":
 			for i in range(5):
-				addGlobal("Ore " + i)
+				addGlobal("Ore " + str(i))
 		"Food":
 			for i in range(5):
-				addGlobal("Ore " + i)
+				addGlobal("Ore " + str(i))
 		"Water":
 			for i in range(5):
-				addGlobal("Ore " + i)
+				addGlobal("Ore " + str(i))
 		"Cement":
 			for i in range(5):
-				addGlobal("Ore " + i)
+				addGlobal("Ore " + str(i))
 		
 
 func _on_Button_pressed():
+	social.view.push_back(get_node("ItemCategories"))
 	$ItemCategories.show()
