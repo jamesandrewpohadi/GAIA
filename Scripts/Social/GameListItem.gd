de48754764364eed2e189fe72c341fe8790b4ac3
James Andrew Pohadi <jamesandrewpohadi@gmail.com>
2019-03-27 00:01:42 +0800

    market integrated with database

--- Scripts/Social/GameListItem.gd
+++ Scripts/Social/GameListItem.gd
@@ -1,21 +1,62 @@
 extends ColorRect
 
 var dict_func
+var unit_price
+var status
+var database = load("res://Scenes/Social/Database.tscn").instance()
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	add_child(database)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 func sell():
-	hide()
+	database.put("game/market/"+$Name.text.to_lower()+"/"+$Category.text,'{"amount":'+str(0)+',"price":'+str(0)+'}')
 	
 func buy():
-	hide()
+	
+	var amount_left = int($Amount.text)-int($EditAmount.text)
+	database.put("game/market/"+$Category.text.to_lower()+"/"+$Name.text,'{"amount":'+str(amount_left)+',"price":'+str(amount_left*unit_price)+'}')
+	print("game/market"+$Category.text.to_lower()+"/"+$Name.text.to_lower())
+	print('{"amount":'+str(amount_left)+',"price":'+str(amount_left*unit_price)+'}')
+	yield(database,"done")
+	if (amount_left == 0):
+		hide()
+		queue_free()
+	else:
+		$Amount.text = str(amount_left)
 
 func _on_Button_pressed():
 	dict_func.my_func.call_func()
+
+
+func _on_EditAmount_text_changed(new_text):
+	match status:
+		"buy":
+			if (int(new_text)>int($Amount.text)):
+				$EditAmount.text = $Amount.text
+			else:
+				$Price.text = str(int(new_text)*unit_price)
+		"sell":
+			$Amount.text = new_text
+
+
+func _on_EditPrice_text_changed(new_text):
+	$Price.text = new_text
+
+
+func _on_EditPrice_text_entered(new_text):
+	database.put("game/market/"+$Name.text.to_lower()+"/"+$Category.text,'{"amount":'+$EditAmount.text+',"price":'+$EditPrice.text+'}')
+	yield(database,"done")
+
+
+func _on_EditAmount_text_entered(new_text):
+	match status:
+		"buy":
+			pass
+		"sell":
+			database.put("game/market/"+$Name.text.to_lower()+"/"+$Category.text,'{"amount":'+$EditAmount.text+',"price":'+$EditPrice.text+'}')
--- Scripts/Social/Market.gd
+++ Scripts/Social/Market.gd
@@ -3,62 +3,88 @@ extends Node2D
 var status = ""
 const GameListItem = preload("res://Scenes/Social/GameListItem.tscn")
 onready var social = get_parent()
+onready var database = load("res://Scenes/Social/Database.tscn").instance()
+onready var main
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	main = get_tree().get_root().get_node("Main")
+	add_child(database)
+	
 
-#func _process(delta):
+func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
+	pass
 
 func setList(category):
 	clearList()
 	match status:
 		"sell":
-			for i in range(10):
-				print(i)
-				addGameItem(category + str(i), category, str(10), "Sell", $ItemList/ScrollContainer/VBoxContainer)
+			
+			for category in ["Academy","Cement","Food","Ore","Water"]:
+				database.query("game/market/"+category.to_lower()+"/"+main.network.player_name)
+				yield(database, "done")
+				var data = database.res
+				addGameItem(category, main.network.name, str(data["amount"]), str(data["price"]), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
 		"buy":
-			for i in range(10):
-				print(i)
-				addGameItem(category + str(i), category, str(10), "Buy", $ItemList/ScrollContainer/VBoxContainer)
+			database.query("game/market/"+category.to_lower())
+			yield(database, "done")
+			var data = database.res
+			print("game/market/"+category.to_lower())
+			print(data)
+			if (!data.empty()):
+				for i in data.keys():
+					if (i!=main.network.player_name):
+						addGameItem(i, category, str(data[i]["amount"]), str(data[i]["price"]), "Buy", $ItemList/ScrollContainer/VBoxContainer)
+#			for i in range(10):
+#				addGameItem(category + str(i), category, str(10), "Buy", $ItemList/ScrollContainer/VBoxContainer)
 
 func setShopList():
 	clearList()
-	for i in range(5):
-		addGameItem("Ore" + str(i), "Ore", str(10), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
-	for i in range(5):
-		addGameItem("Food" + str(i), "Food", str(10), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
-	for i in range(5):
-		addGameItem("Cement" + str(i), "Cement", str(10), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
+	for category in ["Academy","Cement","Food","Ore","Water"]:
+		database.query("game/market/"+category.to_lower()+"/"+main.network.player_name)
+		yield(database, "done")
+		var data = database.res
+		addGameItem(category, main.network.player_name, str(data["amount"]), str(data["price"]), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
+#	for i in range(5):
+#		addGameItem("Ore" + str(i), "Ore", str(10), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
+#	for i in range(5):
+#		addGameItem("Food" + str(i), "Food", str(10), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
+#	for i in range(5):
+#		addGameItem("Cement" + str(i), "Cement", str(10), "Cancel", $ShopList/ScrollContainer/VBoxContainer)
 
 func clearList():
 	for i in $ItemList/ScrollContainer/VBoxContainer.get_children():
 		i.queue_free()
 	#pass
 	
-func addGameItem(name, category, amount, button_text, node):
+func addGameItem(name, category, amount, price,button_text, node):
 	var gameItem = GameListItem.instance()
 	# todo: query from the firebase regarding the item information
-	
+	gameItem.unit_price = int(price)/int(amount)
 	gameItem.get_node("Name").text = name
 	gameItem.get_node("Category").text = category
 	gameItem.get_node("Amount").text = amount
+	gameItem.get_node("Price").text = price
 	gameItem.get_node("Button").text = button_text
+	gameItem.get_node("EditAmount").text = amount
+	gameItem.get_node("EditPrice").text = price
 	match status:
 		"sell":
+			gameItem.status = "sell"
 			var sell_func = funcref(gameItem, "sell")
 			gameItem.dict_func = {
 				my_func = sell_func
 			}
 		"buy":
+			gameItem.status = "buy"
 			var buy_func = funcref(gameItem, "buy")
 			gameItem.dict_func = {
 				my_func = buy_func
 			}
+			gameItem.get_node("EditPrice").hide()
 	node.add_child(gameItem)
 
 func _on_Sell_pressed():
@@ -98,12 +124,6 @@ func _on_Cements_pressed():
 
 func _on_Academies_pressed():
 	setList("Academy")
-	social.view.push_back(get_node("ItemList"))
-	$ItemList.show()
-
-
-func _on_Trees_pressed():
-	setList("Tree")
 	social.view.push_back(get_node("ItemList"))
 	$ItemList.show()
 
